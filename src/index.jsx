d84a623c32b1991b4b3cafce4b4d3186fdea053f
Josephine237 <josefina.novak@gmail.com>
2022-04-26 11:26:20 +0200

    vyřešeno

--- src/index.jsx
+++ src/index.jsx
@@ -1,35 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
-const MenuItem = (props) => {
+const MenuItem = ({onSelect, text}) => {
+  // delší varianta
+  // const handleClick = () => {
+  // onSelect(text);
+  // };
+  
   return (
-    <a href="#" className="menu-item">
-      {props.text}
-    </a>
+    <a href="#" className='menu-item' onClick={() => onSelect(text)}>{text}</a>
   );
 };
 
+
+
 const App = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const [pageTitle, setPageTitle] = useState("Domů");
+
+  // const handleMenuClick = () => {
+  //   // if(menuOpened === true){
+  //   //   setMenuOpened(false)
+  //   // } else if (menuOpened === false){
+  //   //   setMenuOpened(true)
+  //   // }
+  //   setMenuOpened(!menuOpened)
+  // }
+
+  const handleSelectItem = (text) => {
+    setPageTitle(text)
+    setMenuOpened(false)
+  };
+
+
+
   return (
     <>
       <header>
-        <div className="menu">
-          <button className="menu__btn"></button>
+        <div className={`{menu ${menuOpened ? '' : 'menu--closed'}`}>
+          <button 
+          className="menu__btn" 
+          onClick={() => setMenuOpened(!menuOpened)} ></button>
           <div className="menu__items">
-            <MenuItem text="Domů" />
-            <MenuItem text="Naše nabídka" />
-            <MenuItem text="Náš tým" />
-            <MenuItem text="Blog" />
-            <MenuItem text="Kontakt" />
+            <MenuItem onSelect={handleSelectItem} text="Domů"  />
+            <MenuItem text="Naše nabídka" onSelect={handleSelectItem} />
+            <MenuItem text="Náš tým" onSelect={handleSelectItem} />
+            <MenuItem text="Blog" onSelect={handleSelectItem} />
+            <MenuItem text="Kontakt" onSelect={handleSelectItem} />
           </div>
         </div>
       </header>
       <main>
-        <h1>Domů</h1>
+        <h1>{pageTitle}</h1>
       </main>
     </>
   );
 };
 
 render(<App />, document.querySelector('#app'));
+
+
+
